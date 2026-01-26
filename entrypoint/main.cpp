09055b734345ea9c2d6c032367a299a968e68e48
Fei Daderman <pontdade@playgroundsquad.com>
2026-01-26 17:34:58 +0100

    pushing

--- entrypoint/main.cpp
+++ entrypoint/main.cpp
@@ -1,20 +1,28 @@
+#include <GLFW/glfw3.h>
+
 import faelight.log;
 import faelight.signal;
+
 
 int main() {
 
     FL::Log::Config::setLogLevel(FL::Log::eLogLevel::DEBUG);
     FL::Log::info("Hello! Can I have your name? <3");
 
-    FL::Signal<int, int> signal;
-    signal.connect([&](auto num, auto num2) {
-        FL::Log::err("One shot {} and {}", num, num2);
-    }, true);
-    signal.connect([&](auto num, auto num2) {
-       FL::Log::err("Continues {} and {}", num, num2);
-    });
-    signal.trigger(43, 54);
-    signal(54,65);
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Faelight", nullptr, nullptr);
+
+    if (!window) {
+        FL::Log::err("Failed to create window");
+        return 0;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        FL::Log::info("Pressed: {}", glfwGetKey(window, GLFW_KEY_LEFT));
+    }
 
     return 0;
 }
